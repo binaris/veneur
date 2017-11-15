cb4af80ab533ef0866f01665715fd303cd9ea97d
Cory Watson <cory@stripe.com>
2017-11-15 09:28:01 -0800

    Remove that log line

--- span_sink.go
+++ span_sink.go
@@ -357,10 +357,8 @@ func (ls *lightStepSpanSink) Ingest(ssfSpan ssf.SSFSpan) error {
 	endTime := time.Unix(ssfSpan.EndTimestamp/1e9, ssfSpan.EndTimestamp%1e9)
 	finishOpts := opentracing.FinishOptions{FinishTime: endTime}
 	if sp, ok := sp.(*trace.Span); ok {
-		log.Info("TC FINISH")
 		sp.ClientFinishWithOptions(ls.traceClient, finishOpts)
 	} else {
-		log.Info("FINISH")
 		sp.FinishWithOptions(finishOpts)
 	}
 
